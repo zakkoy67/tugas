d6a6246de55901cc35941b7b3224c79a993c05fd
zakkoy67 <118705766+zakkoy67@users.noreply.github.com>
2022-11-29 15:16:14 +0700

    Add files via upload

---
+++ persegipanjang.js
@@ -0,0 +1,7 @@
+const input = require('readline-sync')
+
+var panjang = input.question("masukkan panjang persegi :")
+var lebar = input.question("masukkan lebar persegi :")
+var hitung = panjang * lebar
+
+console.log(hitung)
